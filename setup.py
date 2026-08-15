96a3980e324a700cbf9b13b02e472f9f69a25991
nlscarter <nlscarter@gmail.com>
2026-08-15 22:29:29 +0100

    updated colours

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 FONT_COLOR_MAP = {
-    0: (0, 0, 0),  # Black (Unused/Empty space background)
-    1: (255, 0, 0),  # Red
-    2: (0, 128, 0),  # Green
-    3: (123, 61, 0),  # Brown
-    4: (0, 0, 128),  # Dark Blue / Navy
+    0: (0, 0, 0),  # Black (Unused/Empty space background
+    1: (255, 0, 0),  # Red good
+    2: (0, 128, 0),  # Green good
+    3: (123, 61, 0),  # Brown ok
+    4: (0, 0, 255),  # Dark Blue / Navy
     5: (128, 0, 0),  # Dark Red / Maroon
-    6: (187, 227, 61),  # Lime
+    6: (116, 250, 82),  # Lime
     7: (50, 50, 50),  # Dark Charcoal / Off-Black
     8: (100, 100, 100),  # Gray
-    9: (200, 90, 45),  # Coral / Pinkish Red
+    9: (224, 29, 111),  # Coral / Pinkish Red
     10: (0, 255, 128),  # Light Mint Green
     11: (255, 255, 0),  # Pale Yellow
-    12: (30, 30, 255),  # Light Blue / Lavender
-    13: (128, 255, 255),  # Light Cyan / Sky Blue
-    14: (255, 128, 0),  # Orange
+    12: (100, 100, 255),  # Light Blue / Lavender
+    13: (128, 255, 255),  # Baby Blue
+    14: (255, 146, 40),  # Orange
     15: (255, 255, 255),  # White
 }
